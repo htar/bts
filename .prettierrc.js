87a7788fb02c9f2f3ca7bdc2efc3248e88cf43d2
Taras <khmelovski@gmail.com>
2018-12-22 22:00:24 +0200

    add default  configs

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  useTabs: false,
+  eslintIntegration: true,
+  stylelintIntegration:true,
+  eslintIntegration:true,
+  singleQuote: true
+};
