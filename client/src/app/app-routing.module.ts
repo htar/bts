f1477cba6d6005a96f32bcf320e1e8b62d6798ad
Taras <khmelovski@gmail.com>
2019-02-17 13:46:23 +0200

    add default layouts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
+import { SiteLayoutComponent } from './layouts/site-layout/site-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-	{ path: 'login', component: LoginPageComponent },
+	{ path: '', component: AuthLayoutComponent, children: [
+			{ path: 'login', component: LoginPageComponent }
+	] },
+	// { path: '', component: SiteLayoutComponent, children: [
+
+	// ]},
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './layouts/site-layout/site-layout.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		LoginPageComponent
+		LoginPageComponent,
+		AuthLayoutComponent,
+		SiteLayoutComponent
 	],
 	imports: [
 		BrowserModule,
