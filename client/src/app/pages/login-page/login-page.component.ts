b7ac7df733b61396f6b1f3cd8f72336090d43d91
Taras <khmelovski@gmail.com>
2019-03-25 22:23:26 +0200

    update login

--- client/src/app/pages/login-page/login-page.component.ts
+++ client/src/app/pages/login-page/login-page.component.ts
@@ -1,17 +1,22 @@
 import { AuthService } from './../../services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
 	selector: 'app-login-page',
 	templateUrl: './login-page.component.html',
 	styleUrls: ['./login-page.component.styl'],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 	form: FormGroup;
+	aSub: Subscription
 
-	constructor(private auth: AuthService) {}
+	constructor(private auth: AuthService,
+		private router:Router,
+		private route: ActivatedRoute
+	) {}
 
 	ngOnInit() {
 		this.form = new FormGroup({
@@ -24,6 +29,18 @@ export class LoginPageComponent implements OnInit {
 				Validators.minLength(6),
 			]),
 		});
+		this.route.queryParams.subscribe((params:Params)=>{
+			if (params['registered']) {
+			// You can login in system use your data
+			} else if (params['accessDenied']) {
+			// Please login in system
+			}
+		})
+	}
+	ngOnDestroy(){
+		if (this.aSub) {
+			this.aSub.unsubscribe()
+		}
 	}
 	onSubmit() {
 		// const user = {
@@ -31,11 +48,11 @@ export class LoginPageComponent implements OnInit {
 		// 	password: this.form.value.password,
 		// };
 		this.form.disable()
-		this.auth
+		this.aSub =  this.auth
 		.login(this.form.value)
 		.subscribe(
 			() => {
-				console.info('Login success')
+				this.router.navigate(['/overview'])
 			},
 			errors => {
 				console.warn('Login error')
