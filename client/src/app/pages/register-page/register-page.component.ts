73beeae117b7f5853b665fad30a3ea91feb55208
Taras <khmelovski@gmail.com>
2019-03-25 23:41:30 +0200

    add register service and validations

--- client/src/app/pages/register-page/register-page.component.ts
+++ client/src/app/pages/register-page/register-page.component.ts
@@ -1,15 +1,58 @@
+import { AuthService } from '../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import {
+	FormControl,
+	FormGroup,
+	Validators,
+	FormBuilder,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+// import { PasswordValidation } from '../../shared/password-validation';
 
 @Component({
-  selector: 'app-register-page',
-  templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.styl']
+	selector: 'app-register-page',
+	templateUrl: './register-page.component.html',
+	styleUrls: ['./register-page.component.styl'],
 })
 export class RegisterPageComponent implements OnInit {
+	form: FormGroup;
+	aSub: Subscription;
 
-  constructor() { }
+	constructor(
+		private auth: AuthService,
+		private router: Router,
+		private formBuilder: FormBuilder
+	) {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.form = this.formBuilder.group({
+			email: ['', [Validators.required, Validators.email]],
+			username: ['',[Validators.required, Validators.minLength(4)]],
+			firstName: ['', Validators.required],
+			lastName: ['', Validators.required],
+			address: ['', Validators.required],
+			password: ['',[ Validators.required, Validators.minLength(6)]],
+			// confirmPassword: ['',[PasswordValidation.MatchPassword, Validators.required, Validators.minLength(6)]],
+			date: ['',Validators.required],
+			gender: ['',Validators.required],
+		});
+	}
+	ngOnDestroy() {
+		if (this.aSub) {
+			this.aSub.unsubscribe();
+		}
+	}
+	onSubmit() {
+		this.form.disable();
+		this.aSub = this.auth.register(this.form.value).subscribe(
+			() => {
+				this.router.navigate(['/overview']);
+			},
+			errors => {
+				console.warn('Login error');
+				this.form.enable();
+			}
+		);
+	}
 }
--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../shared/interfaces/User';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -12,10 +13,34 @@ export class AuthService {
 
 	constructor(private http: HttpClient) {}
 
+	register(user: User): Observable<User> {
+		return this.http.post<User>(`/api/auth/register`, user);
+	}
 
 	login(user: User): Observable<{ token: string }> {
 		return this.http.post<{ token: string }>('/api/auth/login', user)
-
+			.pipe(
+				tap(({ token }) => {
+					localStorage.setItem('auth-token', token);
+					this.setToken(token);
+				})
+			);
 	}
 
+	setToken(token: string) {
+		this.token = token;
+	}
+
+	getToken(): string {
+		return this.token;
+	}
+
+	isAuthenticated(): boolean {
+		return !!this.token;
+	}
+
+	logout() {
+		this.setToken(null);
+		localStorage.clear();
+	}
 }
