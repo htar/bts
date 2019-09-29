ecbe737c5213f8d8b5bca69f841b72d354d57c70
Taras <khmelovski@gmail.com>
2019-09-29 12:20:54 +0300

    [FrontEnd] add redirect when you dont auth

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
 	selector: 'app-root',
 	template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+	constructor(private auth: AuthService) {}
+	ngOnInit() {
+		const potentialToken = localStorage.getItem('auth-token');
+		if (potentialToken !== null) {
+			this.auth.setToken(potentialToken);
+		}
+	}
+}
