6c837ca8a90e5d8a958bafae6437a88bb564d0e0
Taras Khmelovskyi <khmelovski@gmail.com>
2019-09-29 14:51:58 +0300

    [FrontEnd] refactoring modules structure

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { NavigationComponent } from './component/navigation/navigation.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SiteLayoutComponent } from './layouts/site-layout/site-layout.component';
 import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 import { TokenInterceptor } from './shared/classes/token.interceptor';
 import { MaterialModule } from './shared/modules/material.module';
 import { LoaderComponent } from './component/loader/loader.component';
+import { AngularModule } from './shared/modules/angular.module';
 
 @NgModule({
 	declarations: [
@@ -29,13 +27,8 @@ import { LoaderComponent } from './component/loader/loader.component';
 		LoaderComponent,
 	],
 	imports: [
-		BrowserModule,
-		FormsModule,
 		AppRoutingModule,
-		BrowserAnimationsModule,
-		ReactiveFormsModule,
-		HttpClientModule,
-
+		AngularModule,
 		MaterialModule
 	],
 	providers: [{
